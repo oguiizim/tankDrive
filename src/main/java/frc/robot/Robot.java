e89d4d887c23efcc3c8b7d7e98491296f4d704b6
Guilherme-Almeida <guilhermecordeiroyt@gmail.com>
2025-06-16 18:38:10 -0300

    done

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,46 +4,118 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
-  SparkMax frontLeft;
-  
+  SparkMax frontLeft, backLeft, frontRight, backRight;
+  XboxController joy;
+
   public Robot() {
     frontLeft = new SparkMax(1, MotorType.kBrushless);
+    backLeft = new SparkMax(2, MotorType.kBrushless);
+    
+    frontRight = new SparkMax(3, MotorType.kBrushless);
+    backRight = new SparkMax(4, MotorType.kBrushless);
+
+    joy = new XboxController(0);
   }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
+
+  public void foward(double speed) {
+    frontLeft.set(speed);
+    backLeft.set(speed);
+
+    frontRight.set(-speed);
+    backRight.set(-speed);
+  }
+
+  public void turn(double speed) {
+    // Speed > 0 = right
+    // Speed < 0 = left
+    frontLeft.set(speed);
+    backLeft.set(speed);
+
+    frontRight.set(speed);
+    backRight.set(speed);
+  }
+
+  public void stop() {
+    frontLeft.stopMotor();
+    backLeft.stopMotor();
+
+    frontRight.stopMotor();
+    backRight.stopMotor();
+  }
+
+  public void turnInDrive(double fdSpeed, double turnSpeed) {
+    frontLeft.set(fdSpeed - turnSpeed);
+    backLeft.set(fdSpeed - turnSpeed);
+
+    frontRight.set(-fdSpeed + turnSpeed);
+    backRight.set(-fdSpeed + turnSpeed);
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if (joy.getRightTriggerAxis() > 0.1) {
+      foward(joy.getRightTriggerAxis());
+    } else if (joy.getLeftTriggerAxis() > 0.1) {
+      foward(-joy.getLeftTriggerAxis());
+    }
+
+    // ATENCAO!!!! NAO SEI SE FUNCIONA ESSA PARTE
+    else if (joy.getLeftX() != 0) {
+      turnInDrive(joy.getRightTriggerAxis(), joy.getLeftX());
+    } else if (joy.getLeftTriggerAxis() > 0.1 && joy.getLeftX() != 0) {
+      turnInDrive(joy.getLeftTriggerAxis(), joy.getLeftX());
+    }
+    //
+
+    else {
+      stop();
+    }
+  }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
