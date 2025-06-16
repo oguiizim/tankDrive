b28cb78892f736367b9ce340721390728cd0879c
Guilherme-Almeida <guilhermecordeiroyt@gmail.com>
2025-06-16 18:31:31 -0300

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
+public class Robot extends TimedRobot {
+  SparkMax frontLeft;
+  
+  public Robot() {
+    frontLeft = new SparkMax(1, MotorType.kBrushless);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
